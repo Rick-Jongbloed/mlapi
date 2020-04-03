739133dc70af80ba36cea93c8556ec126d788831
Rick Jongbloed <ricadelic@gmail.com>
2020-04-04 00:13:38 +0200

    Optimized for speed

--- modules/object_edgetpu_ssd_mobilenet.py
+++ modules/object_edgetpu_ssd_mobilenet.py
@@ -1,8 +1,7 @@
-#import cv2
-#import numpy as np
 import modules.common_params as g
-import os
 import modules.utils as utils
+
+# If debug:
 import datetime
 
 from edgetpu.detection.engine import DetectionEngine
@@ -31,48 +30,31 @@ class Object:
             self.populate_class_labels()
             g.logger.debug('Initializing MobileNet SSD {} TFlife EdgeTPU model'.format(model_type))
             g.logger.debug('edgetpu_ssd_mobilenet_models:{}'.format(model_file_abs_path))
+            
+            # If debug
             start = datetime.datetime.now()
+            
             self.engine = DetectionEngine(model_file_abs_path)
+            
+            # If debug
             diff_time = (datetime.datetime.now() - start).microseconds/1000
             g.logger.debug ('MobileNet SSD v2 EdgeTPU initialization (loading model from disk) took: {} milliseconds'.format(diff_time))
 
             self.initialize = False
           
         # https://coral.ai/docs/reference/edgetpu.detection.engine/#edgetpu.detection.engine.DetectionEngine.detect_with_image
-        #min_threshold = 0.2
-        min_threshold = 0.2
         conf_threshold = g.config['edgetpu_mobilenet_ssd_min_confidence']
-        
-        #detection_candidates = self.engine.detect_with_image (image, threshold=min_threshold, keep_aspect_ratio=True, relative_coord=False, top_k=10)
-        detection_candidates = self.engine.detect_with_image (image, threshold=0.4, keep_aspect_ratio=True, relative_coord=False, top_k=10)
-
-        inference_time = self.engine.get_inference_time()
-        g.logger.debug ('MobileNet SSD v2 EdgeTPU detection took: {} milliseconds'.format(inference_time))
-
-        bbox = []
-        label = []
-        conf = []
-
-        for detected_object in detection_candidates:
-            if detected_object.score >= conf_threshold:
-                bbox.append([int(round(num)) for num in [*detected_object.bounding_box[0], *detected_object.bounding_box[1]]])              # Have to see if i really need to round the numbers here, as 
-                label.append(self.classes[detected_object.label_id])
-                conf.append(detected_object.score)
-                g.logger.info ('object:{} at {} has a acceptable confidence:{} compared to min confidence of: {}, adding'.format(label[-1], bbox[-1], conf[-1], conf_threshold))
-            else:
-                #g.logger.info ('rejecting object:{} at {} because its confidence is :{} compared to min confidence of: {}'.format(self.classes[detected_object.label_id]), [int(round(x)), int(round(y)), int(round(x + w)), int(round(y + h))], confidences[i], g.config['yolo_min_confidence']))
-                g.logger.info ('rejecting object:{} because its confidence is :{} compared to min confidence of: {}'.format(self.classes[detected_object.label_id], detected_object.score, conf_threshold))
-                
+        detection_candidates = self.engine.detect_with_image (image, threshold=conf_threshold, keep_aspect_ratio=True, relative_coord=False, top_k=10)
+        g.logger.debug ('MobileNet SSD v2 EdgeTPU detection took: {} milliseconds'.format(self.engine.get_inference_time()))
+       
+       # Increased speed by running only one processing loop
         detections = []
-
-        for l, c, b in zip(label, conf, bbox):
-            c = "{:.2f}%".format(c * 100)
-            print (c)
+        for detected_object in detection_candidates:
             obj = {
                 'type': 'object',
-                'label': l,
-                'confidence': c,
-                'box': b
+                'label': self.classes[detected_object.label_id],
+                'confidence': "{:.2f}%".format(detected_object.score * 100),
+                'box': [int(round(num)) for num in [*detected_object.bounding_box[0], *detected_object.bounding_box[1]]]
             }
             detections.append(obj)
 
